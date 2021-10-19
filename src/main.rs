d5ba5ccd1ca729e2a68a04f86e9d2a4db6d78875
danielteberian <danielteberian@github.com>
2021-10-18 23:30:59 -0500

    Added a test-case (trivassert), but the test framework is not working. Using Cargo's test command results in some 380 errors. In the meantime, LibertyOS can be compiled without any issues. 0.2.2 also introduces a new function to handle exiting QEMU without having to force it to stop.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::testexec)]
+#![reexport_test_harness_main = "testmain"]
 #![no_main]
 #![no_std]
 
 use core::panic::PanicInfo;
 mod vgabuff;
-//extern crate goodnight;
+
 
 
 #[no_mangle]
 pub extern "C" fn _start() -> !
 {
 	println!("WELCOME TO LIBERTYOS");
+
+	#[cfg(test)]
+	testmain();
 	loop {}
 }
 
@@ -22,6 +26,24 @@ fn panic(info: &PanicInfo) -> !
 	loop {}
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QEMUExitCode
+{
+	Success = 0x10,
+	Failure = 0x11,
+}
+
+pub fn exitqemu(exitcode: QEMUExitCode)
+{
+	use x86_64::instructions::port::Port; //TODO: Use x64.
+	unsafe //TODO: If possible, find a safe way of implementing this.
+	{
+		let mut port = Port::new(0xf4);
+		port.write(exitcode as u32);
+	}
+}
+
 #[cfg(test)]
 fn testexec(tests: &[&dyn Fn()])
 {
@@ -30,4 +52,12 @@ fn testexec(tests: &[&dyn Fn()])
 	{
 		test();
 	}
+}
+
+#[test_case]
+fn trivassert()
+{
+	print!("[TEST] TRIVIAL ASSERTION... ");
+	assert_eq!(1, 1);
+	println!("[TEST] SUCCESS");
 }
