86e674dbca2bad6e2d77bdb95ffafcc510f127e8
danielteberian <danielteberian@gmail.com>
2021-11-08 08:51:40 -0600

    Removed some folders that were supposed to be removed.

--- boot/src/main.rs
+++
@@ -1,9 +0,0 @@
-#![feature(restricted_std)]
-use std::process::Command;
-use bootloader_locator::locate_bootloader;
-
-pub fn main()
-{
-	let bl_manifest = locate_bootloader("bootloader").unwrap();
-	dbg!(bootloader_manifest);
-}
