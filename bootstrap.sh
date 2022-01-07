7867e7bf79e5beab8e697f44ad19fdfb64e05fc0
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2022-01-08 00:48:17 +0100

    Pin 2022-01-07-nightly toolchain
    This ensures everyone uses the same toolchain when building to improve
    reproducibility of issues. Also, updating to a new toolchain becomes an
    intentional change now.
    
    Since cargo takes care of managing the toolchain based on the
    rust-toolchain.toml file we don't need the specifics in the bootstrap.sh
    
    Only install rust if `rustc` is not available in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 clear
 
 echo " _     _ _               _          ___  ____  "
@@ -9,15 +9,13 @@ echo "|_____|_|_.__/ \___|_|   \__|\__, |\___/|____/ "
 echo "                             |___/             "
 echo "By using this script, and by running LibertyOS, you are agreeing to the terms outlined in the LICENSE."
 echo "Installing rustup..."
+if ! rustc --version; then
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-echo "Installing libraries for x86_64-unknown-linux-gnu..."
-rustup target add x86_64-unknown-linux-gnu
-echo "Installing the llvm-tools-preview component..."
-rustup component add llvm-tools-preview
+else
+echo "Rust already installed"
+fi
 echo "Installing the bootimage crate..."
 cargo install bootimage
-echo "Setting the default Rust toolchain to the nightly build..."
-rustup default nightly
 echo "Your system should now be correctly configured to build and run LibertyOS."
 echo "To launch the kernel in a VM, use the cargo run command (requires QEMU)"
 echo "To compile LibertyOS, for use in another VM software, or to run LibertyOS on real hardware, simply use the cargo build command."
